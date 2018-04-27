94c9dc5199fd0ef108bcda40b9c0b0c3c7188c46
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-04-27 10:26:54 -0400

    Create GOODNUMB.cpp

---
+++ GOODNUMB/GOODNUMB.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+const int SIZE = 100001, PRIMESIZE = 304129;
+
+int main() {
+    int T;
+    cin >> T;
+    int minprime[PRIMESIZE];
+    fill(minprime, minprime + PRIMESIZE, 0);
+    for(int i = 2; i < PRIMESIZE; i++) {
+        if(minprime[i] == 0) {
+            for(int j = i; j < PRIMESIZE; j += i) {
+                minprime[j] = i;
+            }
+        }
+    }
+    long long prefix[SIZE];
+    prefix[1] = 0;
+    for(int i = 2; i < SIZE; i++) {
+        map<int, int> count;
+        for(int j = i; j > 1; j /= minprime[j]) {
+            count[minprime[j]]++;
+        }
+        bool squarefree = true, good = false;
+        int current = 1;
+        for(auto& j : count) {
+            if(j.second > 1) {
+                squarefree = false;
+                break;
+            }
+            current *= j.first + 1;
+        }
+        if(squarefree) {
+            count.clear();
+            for(int j = current; j > 1; j /= minprime[j]) {
+                count[minprime[j]]++;
+            }
+            if(minprime[count.size()] == count.size()) {
+                good = true;
+            }
+        }
+        if(good) {
+            prefix[i] = prefix[i - 1] + current;
+        } else {
+            prefix[i] = prefix[i - 1];
+        }
+    }
+    for(int L, R; cin >> L >> R; ) {
+        cout << prefix[R] - prefix[L - 1] << endl;
+    }
+    return 0;
+}
