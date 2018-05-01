ecc4f26ebd23780dc0bffa9569cb3fc97e3763ed
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-05-01 14:47:26 -0400

    Create LPAIR.cpp

---
+++ LPAIR/LPAIR.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <list>
+using namespace std;
+
+int lowbit(int i) {
+    return i & -i;
+}
+
+int get(vector<int>& bit, int index) {
+    int result = 0;
+    while(index > 0) {
+        result += bit[index];
+        index -= lowbit(index);
+    }
+    return result;
+}
+
+void update(vector<int>& bit, int index, int val) {
+    while(index < bit.size()) {
+        bit[index] += val;
+        index += lowbit(index);
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+    int A[N];
+    map<int, int> map;
+    for(int i = 0; i < N; i++) {
+        int j;
+        cin >> j >> A[i];
+        map[j] = A[i];
+    }
+    sort(A, A + N);
+    for(auto& i : map) {
+        i.second = lower_bound(A, A + N, i.second) - A + 1;
+    }
+    long long result = 0;
+    vector<int> bit(N + 1, 0);
+        for(auto it = map.rbegin(); it != map.rend(); it++) {
+        update(bit, it->second, 1);
+        result += get(bit, it->second - 1);
+    }
+    cout << result;
+    return 0;
+}
