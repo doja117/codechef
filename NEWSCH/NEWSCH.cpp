5363b027184e4441faaa64177456608b639f4bf2
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-06-05 16:49:05 -0400

    Update NEWSCH.cpp

--- NEWSCH/NEWSCH.cpp
+++ NEWSCH/NEWSCH.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
-#include <array>
 using namespace std;
 
 const long long MOD = 1000000007;
 
-array<array<long long, 2>, 2> multiply(array<array<long long, 2>, 2>& a, array<array<long long, 2>, 2>& b) {
-    array<array<long long, 2>, 2> result;
-    result[0][0] = (a[0][0] * b[0][0] + a[0][1] * b[1][0]) % MOD;
-    result[0][1] = (a[0][0] * b[0][1] + a[0][1] * b[1][1]) % MOD;
-    result[1][0] = (a[1][0] * b[0][0] + a[1][1] * b[1][0]) % MOD;
-    result[1][1] = (a[1][0] * b[0][1] + a[1][1] * b[1][1]) % MOD;
-    return result;
-}
-
-array<array<long long, 2>, 2> pow(array<array<long long, 2>, 2> a, long long n) {
-    array<array<long long, 2>, 2> result = {{{1, 0}, {0, 1}}};
+long long pow(long long x, int n) {
+    long long result = 1;
     while(n != 0) {
         if(n % 2 == 1) {
-            result = multiply(result, a);
+            result = (result * x) % MOD;
         }
+        x = x * x % MOD;
         n /= 2;
-        a = multiply(a, a);
     }
     return result;
 }
@@ -29,9 +19,13 @@ int main() {
     int T;
     cin >> T;
     for(int n; cin >> n; ) {
-        array<array<long long, 2>, 2> dp {{{2, 3}, {1, 0}}};
-        auto result = pow(dp, n - 2);
-        cout << (result[1][0] * 24 + result[1][1] * 12) % MOD << endl;
+        auto result = pow(3, n);
+        if(n % 2 == 1) {
+            result = (result + MOD - 3) % MOD;
+        } else {
+            result = (result + 3) % MOD;
+        }
+        cout << result << endl;
     }
     return 0;
 }
