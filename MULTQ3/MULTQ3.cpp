4b6b910c1910be575227f52373323d56735ba870
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-06-25 14:54:12 -0400

    Update MULTQ3.cpp

--- MULTQ3/MULTQ3.cpp
+++ MULTQ3/MULTQ3.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 using namespace std;
 
-int* a, n, q, blocksize, **block;
+int* a, n, q, blocksize, **block, *mark;
 
 int sqrt(int n) {
     int begin = 0, end = n;
@@ -17,6 +17,22 @@ int sqrt(int n) {
     return end;
 }
 
+int query(int begin, int end) {
+    int result = 0;
+    for(int i = begin; i < end; i++) {
+        if(a[i] == mark[i / blocksize]) {
+            result++;
+        }
+    }
+    return result;
+}
+
+void update(int begin, int end) {
+    for(int i = begin; i < end; i++) {
+        a[i] = (a[i] + 1) % 3;
+    }
+}
+
 void update(int i) {
     block[i][0] = 0;
     block[i][1] = 0;
@@ -29,34 +45,28 @@ void update(int i) {
 int main() {
     scanf("%d %d", &n, &q);
     blocksize = sqrt(n);
-    int blocknum = (n - 1) / blocksize + 1, mark[blocknum];
+    int blocknum = (n - 1) / blocksize + 1;
     block = new int*[blocknum];
+    mark = new int[blocknum]();
     a = new int[n]();
     for(int i = 0; i < blocknum; i++) {
         block[i] = new int[3];
         block[i][0] = min(blocksize, n - i * blocksize);
         block[i][1] = 0;
         block[i][2] = 0;
-        mark[i] = 0;
     }
     for(int m, l, r; scanf("%d %d %d", &m, &l, &r) > 0; ) {
         int start = l / blocksize + 1, end = r / blocksize;
         switch(m) {
             case 0:
                 if(end <= start) {
-                    for(int i = l; i <= r; i++) {
-                        a[i] = (a[i] + 1) % 3;
-                    }
+                    update(l, r + 1);
                 } else {
-                    for(int i = l; i < start * blocksize; i++) {
-                        a[i] = (a[i] + 1) % 3;
-                    }
+                    update(l, start * blocksize);
                     for(int i = start; i < end; i++) {
                         mark[i] = (mark[i] + 2) % 3;
                     }
-                    for(int i = end * blocksize; i <= r; i++) {
-                        a[i] = (a[i] + 1) % 3;
-                    }
+                    update(end * blocksize, r + 1);
                 }
                 update(l / blocksize);
                 update(r / blocksize);
@@ -64,25 +74,13 @@ int main() {
             case 1:
                 int result = 0;
                 if(end <= start) {
-                    for(int i = l; i <= r; i++) {
-                        if(a[i] == mark[i / blocksize]) {
-                            result++;
-                        }
-                    }
+                    result += query(l, r + 1);
                 } else {
-                    for(int i = l; i < start * blocksize; i++) {
-                        if(a[i] == mark[i / blocksize]) {
-                            result++;
-                        }
-                    }
+                    result += query(l, start * blocksize);
                     for(int i = start; i < end; i++) {
                         result += block[i][mark[i]];
                     }
-                    for(int i = end * blocksize; i <= r; i++) {
-                        if(a[i] == mark[i / blocksize]) {
-                            result++;
-                        }
-                    }
+                    result += query(end * blocksize, r + 1);
                 }
                 printf("%d\n", result);
         }
