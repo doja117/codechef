3455ee1b770de786c397f0ebf5d0b311d4e090f8
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-06-28 12:48:03 -0400

    Update STRBIT.cpp

--- STRBIT/STRBIT.cpp
+++ STRBIT/STRBIT.cpp
@@ -1,40 +1,20 @@
 #include <iostream>
-#include <bitset>
+#include <algorithm>
 using namespace std;
-
-const int BLOCKSIZE = 64;
 
 int main() {
     int T;
     cin >> T;
     for(int n, k; cin >> n >> k; ) {
-        int blocknum = (n - 1) / BLOCKSIZE + 1, result = 0;
-        bitset<BLOCKSIZE> block[blocknum];
+        int result = 0, prefix[n + 2];
         string s;
         cin >> s;
-        for(int i = 0; i < blocknum; i++) {
-            for(int j = i * BLOCKSIZE; j < min(n, (i + 1) * BLOCKSIZE); j++) {
-                block[i][j % BLOCKSIZE] = (s[j] == 'R');
-            }
-        }
-        for(int i = 0; i < n; i++) {
-            if(block[i / BLOCKSIZE][i % BLOCKSIZE]) {
-                int r = min(i + k - 1, n - 1), start = i / BLOCKSIZE + 1, end = r / BLOCKSIZE;
-                if(end <= start) {
-                    for(int j = i; j <= r; j++) {
-                        block[j / BLOCKSIZE].flip(j % BLOCKSIZE);
-                    }
-                } else {
-                    for(int j = i; j < start * BLOCKSIZE; j++) {
-                        block[j / BLOCKSIZE].flip(j % BLOCKSIZE);
-                    }
-                    for(int j = start; j < end; j++) {
-                        block[j].flip();
-                    }
-                    for(int j = end * BLOCKSIZE; j <= r; j++) {
-                        block[j / BLOCKSIZE].flip(j % BLOCKSIZE);
-                    }
-                }
+        fill(prefix, prefix + n + 2, 0);
+        for(int i = 1; i <= n; i++) {
+            prefix[i] += prefix[i - 1];
+            if((prefix[i] + (s[i - 1] == 'R')) % 2 == 1) {
+                prefix[i]++;
+                prefix[min(i + k - 1, n) + 1]--;
                 result++;
             }
         }
