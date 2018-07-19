a496686ec477cbce6c3a4052a225f875a44550dd
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-07-18 21:38:09 -0400

    Create DIGJUMP.cpp

---
+++ DIGJUMP/DIGJUMP.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    vector<int> graph[s.size()];
+    for(int i = 1; i < s.size(); i++) {
+        graph[i].emplace_back(i - 1);
+        graph[i - 1].emplace_back(i);
+    }
+    vector<bool> visited(s.size());
+    visited[0] = true;
+    queue<int> queue;
+    bool flag[10] = {};
+    queue.emplace(0);
+    int result = 0;
+    do {
+        for(auto size = queue.size(); size > 0; size--) {
+            auto front = queue.front();
+            if(front == s.size() - 1) {
+                cout << result;
+                return 0;
+            }
+            queue.pop();
+            for(auto i : graph[front]) {
+                if(!visited[i] && !flag[s[i] - '0']) {
+                    visited[i] = true;
+                    queue.emplace(i);
+                }
+            }
+            if(flag[s[front] - '0']) {
+                continue;
+            }
+            flag[s[front] - '0'] = true;
+            for(int i = 0; i < s.size(); i++) {
+                if(s[i] == s[front] && !visited[i]) {
+                    visited[i] = true;
+                    queue.emplace(i);
+                }
+            }
+        }
+        result++;
+    } while(!queue.empty());
+    return 0;
+}
