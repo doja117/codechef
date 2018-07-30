cd88d1dc216feeb11543fca2c0a2c8710c195a36
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-07-30 12:19:14 -0400

    Update SNSOCIAL.cpp

--- SNSOCIAL/SNSOCIAL.cpp
+++ SNSOCIAL/SNSOCIAL.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-
+ 
 int main() {
     int T;
     cin >> T;
@@ -16,8 +16,9 @@ int main() {
                 }
             }
         }
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        vector<bool> visited[n];
         for(int i = 0; i < n; i++) {
+            visited[i].resize(m);
             for(int j = 0; j < m; j++) {
                 if(a[i][j] == v) {
                     queue.emplace(i, j);
@@ -27,17 +28,16 @@ int main() {
         }
         do {
             for(auto size = queue.size(); size > 0; size--) {
-                auto front = queue.front();
-                queue.pop();
                 for(int i = -1; i <= 1; i++) {
                     for(int j = -1; j <= 1; j++) {
-                        auto x = front.first + i, y = front.second + j;
+                        auto x = queue.front().first + i, y = queue.front().second + j;
                         if(x >= 0 && x < n && y >= 0 && y < m && !visited[x][y]) {
                             visited[x][y] = true;
                             queue.emplace(x, y);
                         } 
                     }
                 }
+                queue.pop();
             }
             result++;
         } while(!queue.empty());
