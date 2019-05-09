50c5971e7bc4eaa0bdfdbe949ca348ae599de32a
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-05-09 16:28:22 -0400

    Update KSPHERES.cpp

--- KSPHERES/KSPHERES.cpp
+++ KSPHERES/KSPHERES.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 const long long MOD = 1000000007;
 
-vector<long long> mul(vector<long long>& a, vector<long long>& b) {
-    vector<long long> result(a.size() + b.size() - 1, 0);
+vector<long long> multiply(vector<long long>& a, vector<long long>& b) {
+    vector<long long> result(a.size() + b.size() - 1);
     for(int i = 0; i < a.size(); i++) {
         for(int j = 0; j < b.size(); j++) {
             result[i + j] = (result[i + j] + a[i] * b[j]) % MOD;
@@ -18,9 +17,7 @@ vector<long long> mul(vector<long long>& a, vector<long long>& b) {
 int main() {
     long long n, m, c;
     cin >> n >> m >> c;
-    long long upper[c + 1], lower[c + 1];
-    fill(upper, upper + c + 1, 0);
-    fill(lower, lower + c + 1, 0);
+    long long upper[c + 1] = {}, lower[c + 1] = {};
     for(long long u; n > 0; n--) {
         cin >> u;
         upper[u]++;
@@ -29,16 +26,15 @@ int main() {
         cin >> l;
         lower[l]++;
     }
-    vector<long long> a;
+    vector<long long> count, polynomial = {1};
     for(long long i = 1; i <= c; i++) {
         if(upper[i] * lower[i] > 0) {
-            a.emplace_back(upper[i] * lower[i]);
+            count.emplace_back(upper[i] * lower[i]);
         }
     }
-    vector<long long> polynomial = {1};
-    for(auto i : a) {
-        vector<long long> b = {-i, 1};
-        polynomial = mul(polynomial, b);
+    for(auto i : count) {
+        vector<long long> j = {-i, 1};
+        polynomial = multiply(polynomial, j);
     }
     for(int i = polynomial.size() - 3; i >= 0; i--) {
         cout << abs(polynomial[i]) << ' ';
