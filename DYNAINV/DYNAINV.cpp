20db3f4a6819d29850761d53e8c535ff7d56460d
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-07-23 20:24:44 -0400

    Update DYNAINV.cpp

--- DYNAINV/DYNAINV.cpp
+++ DYNAINV/DYNAINV.cpp
@@ -5,24 +5,19 @@ int main() {
     int N, Q, result = 0;
     cin >> N >> Q;
     int A[N + 1];
-    for(int i = 1; i <= N; i++) {
+    for (int i = 1; i <= N; i++) {
         cin >> A[i];
     }
-    for(int i = 1; i <= N; i++) {
-        if(A[i] != i) {
-            int j = A[i];
-            A[i] = 0;
-            while(j != 0) {
-                swap(A[j], j);
-                result++;
-            }
-            result--;
+    for (int i = 1; i <= N; i++) {
+        while (A[i] != i) {
+            swap(A[i], A[A[i]]);
+            result++;
         }
     }
     result = result % 2;
-    for(int x, y; cin >> x >> y; ) {
+    for (int x, y; cin >> x >> y; ) {
         result = 1 - result;
-        cout << result << endl;
+        cout << result << '\n';
     }
     return 0;
-}  
+}
