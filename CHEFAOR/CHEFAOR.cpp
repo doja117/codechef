75ce9cc6b6e273b71cf47273fecb3259ef21f6e8
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-08-23 10:08:55 -0400

    Update CHEFAOR.cpp

--- CHEFAOR/CHEFAOR.cpp
+++ CHEFAOR/CHEFAOR.cpp
@@ -14,23 +14,23 @@ int main() {
                 range[i][i + d] = range[i][i + d - 1] | range[i + d][i + d];
             }
         }
-        long long dp[k + 1][n + 1], argmax[k + 1][n + 1];
+        long long dp[k + 1][n + 1], argmin[k + 1][n + 1];
         for (int j = 0; j <= n; j++) {
             dp[0][j] = 0;
-            argmax[k][j] = j;
+            argmin[k][j] = j;
         }
         for (int i = 1; i <= k; i++) {
             dp[i][i] = dp[i - 1][i - 1] + range[i][i];
-            argmax[i - 1][i] = i;
+            argmin[i - 1][i] = i;
         }
         for (int d = 1; d < n; d++) {
             for (int i = 1; i + d <= n && i <= k; i++) {
                 dp[i][i + d] = 0;
-                for (int k = argmax[i - 1][i + d - 1]; k <= argmax[i][i + d]; k++) {
+                for (long long k = argmin[i - 1][i + d - 1]; k <= argmin[i][i + d]; k++) {
                     long long t = dp[i - 1][k - 1] + range[k][i + d];
                     if (t > dp[i][i + d]) {
                         dp[i][i + d] = t;
-                        argmax[i - 1][i + d] = k;
+                        argmin[i - 1][i + d] = k;
                     }
                 }
             }
